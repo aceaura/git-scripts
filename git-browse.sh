f163826cb6889f8db24fa0107f05288f20350c58
aceaura <lzongti@gmail.com>
2026-01-26 13:39:43 +0800

    git lb: interactive commit browser with fzf

--- git-browse.sh
+++ git-browse.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# git-lb: 交互式浏览 git commit 历史
-# 用法: git lb
-# 依赖: fzf
+# git-browse - 交互式浏览 git commit 历史
+# 使用方向键上下选择，Enter 查看详情，Esc/q 退出
 
-git log --oneline --color=always --format="%C(yellow)%h%C(reset) %C(cyan)%ad%C(reset) %s %C(green)(%an)%C(reset)" --date=relative -n 100 | \
+git log --oneline --color=always --format="%C(yellow)%h%C(reset) %C(cyan)%ad%C(reset) %s %C(green)(%an)%C(reset)" --date=relative -n 50 | \
 fzf --ansi --no-sort --reverse --height=100% \
     --preview 'git show --color=always {1}' \
     --preview-window=right:60%:wrap \
