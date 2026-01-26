c5503751b8be2c62a6b7761833f8da374c20bc4b
lzongti@gmail.com <lzongti@gmail.com>
2026-01-26 12:55:17 +0800

    fix: git sa alias list parsing

--- git-sa.sh
+++ git-sa.sh
@@ -20,4 +20,4 @@ echo "  sd    - 删除指定 alias (git sd <name>)"
 echo "  sa    - 显示此帮助信息"
 echo ""
 echo "当前已安装的 alias:"
-git config --global --get-regexp '^alias\.' | sed 's/alias\./  /' | cut -d' ' -f1 | sort
+git config --global --get-regexp '^alias\.' | awk '{print "  " $1}' | sed 's/alias\.//' | sort
