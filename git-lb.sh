e92e35cdfe77e199254c44ba01b6d768394a5801
aceaura <lzongti@gmail.com>
2026-01-26 15:20:16 +0800

    git lb: 优化性能，使用单个awk处理

--- git-lb.sh
+++ git-lb.sh
@@ -4,95 +4,105 @@
 # 依赖: fzf
 # 三级导航: commit -> 文件 -> diff
 
-# 获取 commit hash 和修改行数的映射
-declare -A changes
-while IFS= read -r line; do
-    if [[ $line =~ ^[a-f0-9]{7,} ]]; then
-        hash="${line%% *}"
-    elif [[ $line =~ ([0-9]+)\ insertion ]] || [[ $line =~ ([0-9]+)\ deletion ]]; then
-        ins=0 del=0
-        [[ $line =~ ([0-9]+)\ insertion ]] && ins="${BASH_REMATCH[1]}"
-        [[ $line =~ ([0-9]+)\ deletion ]] && del="${BASH_REMATCH[1]}"
-        changes[$hash]=$((ins + del))
-    fi
-done < <(git log -n 100 --format="%h" --shortstat --reverse)
-
-# 根据修改行数计算颜色 (内联版本)
-calc_color() {
-    local tc=$1
-    if [ $tc -le 47 ]; then
-        echo $((232 + tc / 2))
-    elif [ $tc -le 100 ]; then
-        echo $((229 - (tc - 48) * 9 / 52))
-    elif [ $tc -le 200 ]; then
-        echo $((220 - (tc - 100) * 12 / 100))
-    elif [ $tc -le 500 ]; then
-        echo $((208 - (tc - 200) * 6 / 300))
-    else
-        local c=$((202 - (tc - 500) * 6 / 500))
-        [ $c -lt 196 ] && c=196
-        echo $c
-    fi
-}
-
-# 生成带颜色的 commit 列表
+# 生成带颜色的 commit 列表（优化版：单个 awk 处理）
 generate_commits() {
-    git log --oneline --color=always --format="%C(yellow)%h%C(reset) %C(cyan)%ad %C(green)%an%C(reset) %s" --date=relative -n 100 --reverse | while IFS= read -r line; do
-        hash=$(echo "$line" | sed -n 's/.*\[33m\([a-f0-9]\{7,\}\).*/\1/p')
-        tc=${changes[$hash]:-0}
-        color=$(calc_color $tc)
+    {
+        git log -n 100 --format="%h" --shortstat --reverse
+        echo "---SEPARATOR---"
+        git log --oneline --color=always --format="%C(yellow)%h%C(reset) %C(cyan)%ad %C(green)%an%C(reset) %s" --date=relative -n 100 --reverse
+    } | awk '
+    BEGIN { phase=1; idx=0 }
+    /^---SEPARATOR---$/ { phase=2; next }
+    phase==1 && /^[a-f0-9]{7,}/ {
+        idx++
+        hashes[idx]=$1
+        changes[idx]=0
+        next
+    }
+    phase==1 && /insertion|deletion/ {
+        tc=0
+        if(match($0, /[0-9]+ insertion/)) tc+=substr($0, RSTART, RLENGTH-10)
+        if(match($0, /[0-9]+ deletion/)) tc+=substr($0, RSTART, RLENGTH-9)
+        changes[idx]=tc
+        next
+    }
+    phase==2 && /^\033/ {
+        line=$0
+        hash=""
+        if(match(line, /\033\[33m[a-f0-9]+/)) {
+            hash=substr(line, RSTART+5, RLENGTH-5)
+        }
         
-        echo "$line" | awk -v color="$color" '{
-            line=$0
-            if(match(line, /\033\[32m/)) {
-                p1=substr(line, 1, RSTART+RLENGTH-1)
-                r1=substr(line, RSTART+RLENGTH)
-                if(match(r1, /\033\[0?m/)) {
-                    author=substr(r1, 1, RSTART-1)
-                    s1=substr(r1, RSTART)
-                    if(match(author, /^[a-zA-Z0-9]+/)) {
-                        short=substr(author, 1, RLENGTH)
-                        if(length(short)>8) short=substr(short,1,8)
-                        line=p1 short s1
-                    }
+        tc=0
+        for(i=1;i<=idx;i++) {
+            if(hashes[i]==hash) { tc=changes[i]; break }
+        }
+        
+        if(tc <= 47) {
+            color = 232 + int(tc / 2)
+            if(color > 255) color = 255
+        } else if(tc <= 100) {
+            color = 229 - int((tc - 48) * 9 / 52)
+        } else if(tc <= 200) {
+            color = 220 - int((tc - 100) * 12 / 100)
+        } else if(tc <= 500) {
+            color = 208 - int((tc - 200) * 6 / 300)
+        } else {
+            color = 202 - int((tc - 500) * 6 / 500)
+            if(color < 196) color = 196
+        }
+        
+        if(match(line, /\033\[32m/)) {
+            p1=substr(line, 1, RSTART+RLENGTH-1)
+            r1=substr(line, RSTART+RLENGTH)
+            if(match(r1, /\033\[0?m/)) {
+                author=substr(r1, 1, RSTART-1)
+                s1=substr(r1, RSTART)
+                if(match(author, /^[a-zA-Z0-9]+/)) {
+                    short=substr(author, 1, RLENGTH)
+                    if(length(short)>8) short=substr(short,1,8)
+                    line=p1 short s1
                 }
             }
-            lastpos=0; tmp=line
-            while(match(tmp, /\033\[0?m/)) {
-                lastpos=lastpos+RSTART+RLENGTH-1
-                tmp=substr(tmp, RSTART+RLENGTH)
-            }
-            if(lastpos>0 && lastpos<length(line)) {
-                prefix=substr(line,1,lastpos)
-                desc=substr(line,lastpos+1)
-                if(desc!="") line=prefix "\033[38;5;" color "m" desc "\033[0m"
-            }
-            print line
-        }'
-    done
+        }
+        
+        lastpos=0; tmp=line
+        while(match(tmp, /\033\[0?m/)) {
+            lastpos=lastpos+RSTART+RLENGTH-1
+            tmp=substr(tmp, RSTART+RLENGTH)
+        }
+        if(lastpos>0 && lastpos<length(line)) {
+            prefix=substr(line,1,lastpos)
+            desc=substr(line,lastpos+1)
+            if(desc!="") line=prefix "\033[38;5;" color "m" desc "\033[0m"
+        }
+        print line
+    }'
 }
 
-# 生成文件列表（带颜色）- 输出格式: filename|status_display
+# 生成文件列表（带颜色）
 generate_files() {
     local hash=$1
-    git diff-tree --no-commit-id --numstat -r "$hash" | while read add del filename; do
-        [ -z "$filename" ] && continue
-        [ "$add" = "-" ] && add=0
-        [ "$del" = "-" ] && del=0
-        tc=$((add + del))
-        color=$(calc_color $tc)
+    git diff-tree --no-commit-id --numstat -r "$hash" | awk '
+    {
+        add=$1; del=$2; filename=$3
+        if(filename=="") next
+        if(add=="-") add=0
+        if(del=="-") del=0
+        tc=add+del
         
-        if [ "$del" = "0" ] && [ "$add" != "0" ]; then
-            status="\033[32m[+$add]"
-        elif [ "$add" = "0" ] && [ "$del" != "0" ]; then
-            status="\033[31m[-$del]"
-        else
-            status="\033[33m[+$add -$del]"
-        fi
+        if(tc <= 47) color = 232 + int(tc / 2)
+        else if(tc <= 100) color = 229 - int((tc - 48) * 9 / 52)
+        else if(tc <= 200) color = 220 - int((tc - 100) * 12 / 100)
+        else if(tc <= 500) color = 208 - int((tc - 200) * 6 / 300)
+        else { color = 202 - int((tc - 500) * 6 / 500); if(color < 196) color = 196 }
         
-        # 格式: filename TAB status colored_filename
-        printf "%s\t%b \033[38;5;%dm%s\033[0m\n" "$filename" "$status" "$color" "$filename"
-    done
+        if(del==0 && add>0) status="\033[32m[+" add "]"
+        else if(add==0 && del>0) status="\033[31m[-" del "]"
+        else status="\033[33m[+" add " -" del "]"
+        
+        printf "%s\t%s \033[38;5;%dm%s\033[0m\n", filename, status, color, filename
+    }'
 }
 
 # 第一级：选择 commit
