192a71b0dc116076936f5ce70d7ccacc0e265748
aceaura <lzongti@gmail.com>
2026-01-26 14:01:07 +0800

    git lf: 根据修改行数显示不同颜色深浅

--- git-lf.sh
+++ git-lf.sh
@@ -7,20 +7,15 @@ cols=${COLUMNS:-$(tput cols 2>/dev/null || echo 120)}
 git -c color.ui=always log --oneline -n 100 --format="%C(yellow)%h%C(reset) %C(cyan)%ad %C(green)%an%C(reset) %s" --date=relative --stat --reverse "$@" | awk -v cols="$cols" '
 BEGIN {
     indent="        "
-    first=1
     maxlines=3
     indentlen=8
+    idx=0
 }
 /^\033/ {
-    if(line) {
-        print line
-        printFiles()
-        if(!first) print ""
-    }
-    first=0
-    # 截断作者名：取第一个非字母数字前的部分，最多8字符
+    idx++
+    commits[idx]=$0
+    # 截断作者名
     line=$0
-    # 匹配绿色作者名并截断: ESC[32m....ESC[m 或 ESC[0m
     if(match(line, /\033\[32m/)) {
         prefix=substr(line, 1, RSTART+RLENGTH-1)
         rest=substr(line, RSTART+RLENGTH)
@@ -30,98 +25,115 @@ BEGIN {
             if(match(author, /^[a-zA-Z0-9]+/)) {
                 shortname=substr(author, 1, RLENGTH)
                 if(length(shortname)>8) shortname=substr(shortname,1,8)
-                line=prefix shortname suffix
+                commits[idx]=prefix shortname suffix
             }
         }
     }
-    delete arr
-    n=0
-    stat=""
+    files[idx]=""
+    stats[idx]=""
+    changes[idx]=0
+    next
 }
-!/^\033/ && /file.*changed/ {
-    stat=$0
+/file.*changed/ {
+    stats[idx]=$0
+    tmp=$0
+    if(match(tmp, /[0-9]+ insertion/)) {
+        changes[idx] += substr(tmp, RSTART, RLENGTH-10)
+    }
+    if(match(tmp, /[0-9]+ deletion/)) {
+        changes[idx] += substr(tmp, RSTART, RLENGTH-9)
+    }
+    next
 }
-!/^\033/ && !/file.*changed/ && / \| / {
+/ \| / {
     split($1,a," ")
-    arr[++n]=a[1]
+    if(files[idx]=="") files[idx]=a[1]
+    else files[idx]=files[idx]" "a[1]
+    next
 }
 END {
-    if(line) {
-        print line
-        printFiles()
+    for(i=1; i<=idx; i++) {
+        if(i>1) print ""
+        printCommit(i)
+        printFiles(i)
     }
 }
-function printFiles() {
-    if(n==0) return
+function printCommit(i) {
+    tc=changes[i]
+    # 颜色渐变：灰->白->黄->橙->红
+    if(tc <= 5) color = 240       # 深灰
+    else if(tc <= 15) color = 245 # 灰
+    else if(tc <= 30) color = 252 # 浅灰/白
+    else if(tc <= 60) color = 228 # 浅黄
+    else if(tc <= 100) color = 220 # 黄
+    else if(tc <= 200) color = 214 # 橙
+    else if(tc <= 500) color = 208 # 深橙
+    else color = 196              # 红
+    
+    line=commits[i]
+    lastpos = 0
+    tmpline = line
+    while(match(tmpline, /\033\[0?m/)) {
+        lastpos = lastpos + RSTART + RLENGTH - 1
+        tmpline = substr(tmpline, RSTART + RLENGTH)
+    }
+    if(lastpos > 0 && lastpos < length(line)) {
+        prefix = substr(line, 1, lastpos)
+        desc = substr(line, lastpos + 1)
+        if(desc != "") {
+            line = prefix "\033[38;5;" color "m" desc "\033[0m"
+        }
+    }
+    print line
+}
+function printFiles(i) {
+    if(files[i]=="") return
     maxw=cols-indentlen
+    stat=stats[i]
     gsub(/^ +/,"",stat)
     statlen=length(stat)
     
-    # 构建所有行，最后一行要预留 stat 空间
+    # 分割文件列表
+    n=split(files[i], arr, " ")
+    
     delete lines
     linecount=0
     cur=""
     
-    for(i=1;i<=n;i++) {
-        f="["arr[i]"]"
-        if(cur=="") {
-            cur=f
-        } else if(length(cur" "f)>maxw) {
+    for(j=1;j<=n;j++) {
+        f="["arr[j]"]"
+        if(cur=="") cur=f
+        else if(length(cur" "f)>maxw) {
             lines[++linecount]=cur
             cur=f
-        } else {
-            cur=cur" "f
-        }
+        } else cur=cur" "f
     }
     if(cur!="") lines[++linecount]=cur
     
-    # 重新计算：最后一行需要加上 stat，如果放不下就把 stat 单独放一行
-    # 检查最后一行加上 stat 是否超宽
     if(linecount>0 && length(lines[linecount])+1+statlen>maxw) {
-        # stat 放不下，需要重新分配
-        # 把最后一行的部分文件移到新行，或者 stat 单独一行
         lastline=lines[linecount]
-        # 尝试拆分最后一行
         delete newarr
-        split(lastline, newarr, " ")
+        nn=split(lastline, newarr, " ")
         newlast=""
-        for(j in newarr) {
+        for(j=1;j<=nn;j++) {
             if(newlast=="") newlast=newarr[j]
-            else if(length(newlast" "newarr[j])+1+statlen<=maxw) {
-                newlast=newlast" "newarr[j]
-            } else {
-                # 放不下了，把之前的作为新行
+            else if(length(newlast" "newarr[j])+1+statlen<=maxw) newlast=newlast" "newarr[j]
+            else {
                 lines[linecount]=newlast
                 linecount++
                 newlast=newarr[j]
             }
         }
-        if(newlast!="") {
-            lines[linecount]=newlast
-        }
+        if(newlast!="") lines[linecount]=newlast
     }
     
-    # 现在输出，最多3行
     if(linecount>maxlines) {
-        # 超过3行，前2行正常打印，第3行截断
-        for(i=1;i<=2;i++) {
-            print indent"\033[35m"lines[i]"\033[0m"
-        }
-        # 第3行：截断 + ... + stat
+        for(j=1;j<=2;j++) print indent"\033[35m"lines[j]"\033[0m"
         available=maxw-statlen-5
-        if(available>0) {
-            print indent"\033[35m"substr(lines[3],1,available)"... \033[33m"stat"\033[0m"
-        } else {
-            print indent"\033[35m""... \033[33m"stat"\033[0m"
-        }
+        if(available>0) print indent"\033[35m"substr(lines[3],1,available)"... \033[33m"stat"\033[0m"
+        else print indent"\033[35m""... \033[33m"stat"\033[0m"
     } else {
-        # <=3行，正常打印
-        for(i=1;i<linecount;i++) {
-            print indent"\033[35m"lines[i]"\033[0m"
-        }
-        # 最后一行加 stat
-        if(linecount>0) {
-            print indent"\033[35m"lines[linecount]" \033[33m"stat"\033[0m"
-        }
+        for(j=1;j<linecount;j++) print indent"\033[35m"lines[j]"\033[0m"
+        if(linecount>0) print indent"\033[35m"lines[linecount]" \033[33m"stat"\033[0m"
     }
 }'
