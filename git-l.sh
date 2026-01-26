4dac0dda8d95c3f5d220bdd8461298537dee91a2
lzongti@gmail.com <lzongti@gmail.com>
2026-01-26 13:31:25 +0800

    git l/lf: show commits in reverse order (oldest first)

--- git-l.sh
+++ git-l.sh
@@ -2,4 +2,4 @@
 # git-l: 显示简洁的 git log，带图形和装饰
 # 用法: git l
 
-git log --graph --decorate -n 20 --format='%C(yellow)%h %C(cyan)%ar%C(reset)%C(auto)%d%C(reset) %s %C(green)(%an)%C(reset)'
+git log --graph --decorate -n 20 --format='%C(yellow)%h %C(cyan)%ar%C(reset)%C(auto)%d%C(reset) %s %C(green)(%an)%C(reset)' --reverse "$@"
